b0edbdbd844a08e7cbfb2f8cb826abc2ae6e1c07
yeeuniii <anna862700@gmail.com>
2024-03-21 18:01:46 +0900

    refactor/pong-together/backend#91: 사용자 모델에서 게임 전적 필드 삭제

--- srcs/users/models.py
+++ srcs/users/models.py
@@ -21,9 +21,6 @@ class User(AbstractUser):
 
     intra_id = models.CharField(unique=True)
     image = models.URLField()
-    win_count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    lose_count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    game_count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     language = models.CharField(max_length=2, choices=LanguageChoice, default=LanguageChoice.KOREAN)
     otp_secret_key = models.CharField(max_length=32, default=pyotp.random_base32)
     game_status = models.CharField(max_length=12, choices=GameStatusChoice, default=GameStatusChoice.DISCONNECTED)
